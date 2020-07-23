bdd20c0f95afb293a3c5306cff5ada8496a10530
Sparky-Makes <68678594+Sparky-Makes@users.noreply.github.com>
2020-07-23 10:57:54 +0530

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(new Vector3(0,0,10));
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(new Vector3(0,0,-10));
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector3(-10,0,0));
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector3(10,0,0));
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0,20,0));
+        }
+    }
+}
